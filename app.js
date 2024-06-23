de474c3345fc8b45d07e1af54ffc2b8bd3016d2b
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2024-06-23 21:36:45 +0530

    Auth2O

--- app.js
+++ app.js
@@ -1,33 +1,95 @@
-
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyparser = require("body-parser");
-require("dotenv").config();
-const PORT = process.env.PORT || 8080;
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
-app.use(express.static('public'));
+
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyparser.urlencoded({
+app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 
-// const app = express(); 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-   
-// app.get('/', (req, res) => { 
-//    res.sendFile('signin');   
-// }); 
-const router = require("./src/routes");
-   
-app.use(express.json())
-app.use(cookieParser())
-app.use(router);
-app.get('/', (req, res) => {
-    res.render('signin');
+mongoose.connect("mongodb+srv://admin-priyanshu:Ufm3kPz1qvDEoZw6@cluster0.9umcloi.mongodb.net", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
 });
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/homepage",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+app.get("/", function (req, res) {
+    res.render("signin");
+});
+
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ["profile"] })
+);
+app.get("/auth/google/homepage",
+    passport.authenticate('google', { failureRedirect: "signin" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("homepage");
+    });
+
+app.get("/submit", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("homepage");
+    } else {
+        res.redirect("about");
+    }
+});
+app.listen(3000, function() {
+    console.log("Server started on port 3000.");
+  });
+  
