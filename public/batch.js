03e9208c6a0dcc0f5d7a8db6ac935ecce30c3f62
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2024-07-01 17:13:34 +0530

    Updated batches page

--- public/batch.js
+++ public/batch.js
@@ -3,12 +3,33 @@ $(document).ready(function () {
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
-        $('.dropdown-content').removeClass('onscreen');
-    });
-    $('.dropdown').click(function () {
-        $('.dropdown-content').toggleClass("onscreen");
     });
 });
+
+$(document).ready(function () {
+    $('.menu-btn').click(function () {
+        $('.menu').toggleClass('active');
+    });
+});
+
+(function ($) {
+    $(function () {
+        $('nav ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.dropdown').toggle();
+            $('.dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function () {
+            $('.dropdown').hide();
+        });
+        $('#nav-toggle').click(function () {
+            $('nav ul').slideToggle();
+        });
+        $('#nav-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
+    });
+})(jQuery);
 
 
 
