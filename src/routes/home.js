5e1d90adc5fc7064b3639a7b0e699d8e88853b76
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2024-07-06 13:30:50 +0530

    Updated form

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,6 +4,8 @@ const router = express.Router();
 router.get('/home', (req, res) => {
   const userjwt=req.cookies.jwt ;
   const usergoog=req.user ;
+  // const image=req.user.img;
+  // console.log(image); 
   res.render('homepage',{userjwt,usergoog});
 });
 
