0e91428fc32e8be272f7c5c52b7b6001f86ac0dd
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2024-07-08 01:02:45 +0530

    form fix

--- public/script/form.js
+++ public/script/form.js
@@ -1,3 +1,34 @@
+$(document).ready(function () {
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
+  });
+  
+  $(document).ready(function () {
+    $('.menu-btn').click(function () {
+        $('.menu').toggleClass('active');
+    });
+  });
+  
+  (function ($) {
+    $(function () {
+        $('nav ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.dropdown').toggle();
+            $('.dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function () {
+            $('.dropdown').hide();
+        });
+        $('#nav-toggle').click(function () {
+            $('nav ul').slideToggle();
+        });
+        $('#nav-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
+    });
+  })(jQuery);
 document.getElementById('browseButton').addEventListener('click', function() {
     document.getElementById('profilePicture').click();
 });
@@ -91,36 +122,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 });
-
-
-$(document).ready(function () {
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass('active');
-        $('.menu-btn i').toggleClass('active');
-    });
-  });
-  
-  $(document).ready(function () {
-    $('.menu-btn').click(function () {
-        $('.menu').toggleClass('active');
-    });
-  });
-  
-  (function ($) {
-    $(function () {
-        $('nav ul li a:not(:only-child)').click(function (e) {
-            $(this).siblings('.dropdown').toggle();
-            $('.dropdown').not($(this).siblings()).hide();
-            e.stopPropagation();
-        });
-        $('html').click(function () {
-            $('.dropdown').hide();
-        });
-        $('#nav-toggle').click(function () {
-            $('nav ul').slideToggle();
-        });
-        $('#nav-toggle').on('click', function () {
-            this.classList.toggle('active');
-        });
-    });
-  })(jQuery);
